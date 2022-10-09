92dc8abc447e7fcd594b44fc91f5a9bedf669fcd
Юрий Криворучко <django.python4132@gmail.com>
2022-10-09 13:16:26 +0300

    Added DeadEnemyCompositeCounter.cs for compiling dead enemy counters; Added DeadEnemyCompositeCounterMonoAdapter.cs for working with DeadEnemyCompositeCounter.cs in game scene.

---
+++ Assets/Code/Scripts/DeadEnemyCompositeCounter.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BoxDefence.UI
+{
+    public class DeadEnemyCompositeCounter : IDeadEnemyCounting
+    {
+        #region Fields
+
+        private List<IDeadEnemyCounting> _deadEnemyCountings;
+
+        #endregion
+
+        #region Actions
+
+        public event Action OnAddDeadEnemy;
+        public event Action OnRemoveDeadEnemy;
+
+        #endregion
+
+        #region Constructor
+
+        public DeadEnemyCompositeCounter()
+        {
+            _deadEnemyCountings = new List<IDeadEnemyCounting>();
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public void AddDeadEnemyCounting(IDeadEnemyCounting enemyCounting)
+        {
+            _deadEnemyCountings.Add(enemyCounting);
+            enemyCounting.OnAddDeadEnemy += CallEventOnAddDeadEnemy;
+            enemyCounting.OnRemoveDeadEnemy += CallEventOnRemoveDeadEnemy;
+        }
+        public void RemoveDeadEnemyCounting(IDeadEnemyCounting enemyCounting)
+        {
+            _deadEnemyCountings.Remove(enemyCounting);
+            enemyCounting.OnAddDeadEnemy -= CallEventOnAddDeadEnemy;
+            enemyCounting.OnRemoveDeadEnemy -= CallEventOnRemoveDeadEnemy;
+        }
+        public int GetDeadEnemyCount()
+        {
+            int deadEnemyCount = 0;
+
+            foreach (IDeadEnemyCounting deadEnemyCounting in _deadEnemyCountings)
+                deadEnemyCount += deadEnemyCounting.GetDeadEnemyCount();
+
+            return deadEnemyCount;
+        }
+        public int GetMaxDeadEnemyCount()
+        {
+            int maxDeadEnemyCount = 0;
+
+            foreach (IDeadEnemyCounting deadEnemyCounting in _deadEnemyCountings)
+                maxDeadEnemyCount += deadEnemyCounting.GetMaxDeadEnemyCount();
+
+            return maxDeadEnemyCount;
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private void CallEventOnAddDeadEnemy()
+        {
+            OnAddDeadEnemy.Invoke();
+        }
+        private void CallEventOnRemoveDeadEnemy()
+        {
+            OnRemoveDeadEnemy.Invoke();
+        }
+
+        #endregion
+    }
+}
---
+++ Assets/Code/Scripts/DeadEnemyCompositeCounterMonoAdapter.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System;
+
+namespace BoxDefence.UI
+{
+    public class DeadEnemyCompositeCounterMonoAdapter : MonoBehaviour, IDeadEnemyCounting
+    {
+        #region Fields
+
+        private DeadEnemyCompositeCounter _deadEnemyCounting;
+
+        #endregion
+
+        #region Actions
+
+        public event Action OnAddDeadEnemy;
+        public event Action OnRemoveDeadEnemy;
+
+        #endregion
+
+        #region Unity Methods
+
+        private void Awake()
+        {
+            _deadEnemyCounting = new DeadEnemyCompositeCounter();
+        }
+        private void OnEnable()
+        {
+            if (_deadEnemyCounting == null)
+                throw new Exception("On enable object; _deadEnemyCounting is null!");
+
+            _deadEnemyCounting.OnAddDeadEnemy += CallEventOnAddDeadEnemy;
+            _deadEnemyCounting.OnRemoveDeadEnemy += CallEventOnRemoveDeadEnemy;
+        }
+        private void OnDisable()
+        {
+            if (_deadEnemyCounting == null)
+                throw new Exception("On disable object; _deadEnemyCounting is null!");
+
+            _deadEnemyCounting.OnAddDeadEnemy -= CallEventOnAddDeadEnemy;
+            _deadEnemyCounting.OnRemoveDeadEnemy -= CallEventOnRemoveDeadEnemy;
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public void AddDeadEnemyCounting(IDeadEnemyCounting enemyCounting)
+        {
+            _deadEnemyCounting.AddDeadEnemyCounting(enemyCounting);
+        }
+        public void RemoveDeadEnemyCounting(IDeadEnemyCounting enemyCounting)
+        {
+            _deadEnemyCounting.RemoveDeadEnemyCounting(enemyCounting);
+        }
+        public int GetDeadEnemyCount()
+        {
+            return _deadEnemyCounting.GetDeadEnemyCount();
+        }
+        public int GetMaxDeadEnemyCount()
+        {
+            return _deadEnemyCounting.GetMaxDeadEnemyCount();
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private void CallEventOnAddDeadEnemy()
+        {
+            OnAddDeadEnemy.Invoke();
+        }
+        private void CallEventOnRemoveDeadEnemy()
+        {
+            OnRemoveDeadEnemy.Invoke();
+        }
+
+        #endregion
+    }
+}
